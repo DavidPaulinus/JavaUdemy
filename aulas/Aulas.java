35891820538a7fbe59051c55a1a996c1b1214e17
Davi Paulino <davipm@hotmail.com>
2022-09-12 12:30:15 -0300

    instanciando data-hora

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,10 +1,38 @@
 package aulas;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Aulas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		LocalDate ld1 = LocalDate.now();
+		LocalDateTime ld2 = LocalDateTime.now();
+		Instant ld3 = Instant.now();
+
+		LocalDate ld4 = LocalDate.parse("2002-03-15");
+		Instant ld5 = Instant.parse("2002-03-15T01:30:00.0Z");
+		Instant ld6 = Instant.parse("2002-03-15T01:30:00.0-03:00");
+		LocalDate ld7 = LocalDate.parse("26/09/2003", dtf);
+		LocalDate ld8 = LocalDate.parse("26/09/2053", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+		System.out.println("LocalDate        " + ld1);
+		System.out.println("LocalDateTime    " + ld2);
+		System.out.println("Instant          " + ld3); // London time
+		System.out.println("LocalDate String " + ld4);
+		System.out.println("Instant          " + ld5);
+		System.out.println("Instant          " + ld6);
+		System.out.println("LocalDate        " + ld7);
+		System.out.println("LocalDate        " + ld8);
+
+		sc.close();
 	}
 
 }
--- exercicios/Exercicios.java
+++ exercicios/Exercicios.java
@@ -1,56 +1,9 @@
 package exercicios;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Exercicios {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Random rand = new Random();
-
-		System.out.print("Coluna ");
-		int coluna = sc.nextInt();
-		System.out.print("Linha ");
-		int linha = sc.nextInt();
-
-		int[][] valor = new int[coluna + 1][linha + 1];
-
-		for (int i = 0; i < linha; i++) {
-			for (int j = 0; j < coluna; j++) {
-				valor[i][j] = rand.nextInt(0, 10);
-				System.out.print(valor[i][j] + " ");
-			}
-			System.out.println();
-		}
-
-		System.out.print("Número ");
-		int num = sc.nextInt();
-		for (int i = 0; i < linha; i++) {
-			for (int j = 0; j < coluna; j++) {
-				if (valor[i][j] == num) {
-					System.out.println("\nPosition - \n   Column: " + (j + 1) + "\n   Line: " + (i + 1));
-					if (j > 0) {
-						System.out.println("Left - " + valor[i][j - 1]);
-
-					}
-					if (j + 1 < coluna) {
-						System.out.println("Right - " + valor[i][j + 1]);
-
-					}
-					if (i > 0) {
-						System.out.println("Up - " + valor[i - 1][j]);
-
-					}
-					if (i < coluna) {
-						System.out.println("Down - " + valor[i + 1][j]);
-
-					}
-				}
-			}
-		}
-
-		sc.close();
+		
 	}
 
 }
