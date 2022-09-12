324b272d5e9578d6d0f3dac8a31e2d6c254c0c38
Davi Paulino <davipm@hotmail.com>
2022-09-12 13:14:51 -0300

    Convertendo dat-hora

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -2,37 +2,27 @@ package aulas;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
 public class Aulas {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-		LocalDate ld1 = LocalDate.now();
-		LocalDateTime ld2 = LocalDateTime.now();
-		Instant ld3 = Instant.now();
-
-		LocalDate ld4 = LocalDate.parse("2002-03-15");
-		Instant ld5 = Instant.parse("2002-03-15T01:30:00.0Z");
-		Instant ld6 = Instant.parse("2002-03-15T01:30:00.0-03:00");
-		LocalDate ld7 = LocalDate.parse("26/09/2003", dtf);
-		LocalDate ld8 = LocalDate.parse("26/09/2053", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
-		System.out.println("LocalDate        " + ld1);
-		System.out.println("LocalDateTime    " + ld2);
-		System.out.println("Instant          " + ld3); // London time
-		System.out.println("LocalDate String " + ld4);
-		System.out.println("Instant          " + ld5);
-		System.out.println("Instant          " + ld6);
-		System.out.println("LocalDate        " + ld7);
-		System.out.println("LocalDate        " + ld8);
-
-		sc.close();
+		
+		LocalDate ld1=LocalDate.parse("2003-09-26");
+		System.out.println(ld1);
+		
+		DateTimeFormatter ldf=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(ld1.format(ldf));
+		System.out.println(ldf.format(ld1));
+		System.out.println(ld1.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		DateTimeFormatter ldf2=DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		
+		Instant ins=Instant.now();
+		
+		System.out.println(ldf2.format(ins));
+		
 	}
 
 }
