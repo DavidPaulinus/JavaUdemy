d8d3f4c46105d5a163bb73b00f19cbe3b1a145a5
Davi Paulino <davipm@hotmail.com>
2022-10-19 11:32:23 -0300

    manipulating folders

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,7 +1,7 @@
 package aulas;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Aulas {
@@ -10,19 +10,19 @@ public class Aulas {
 
 		String path = "C:\\Users\\AdaumirM\\Downloads\\minhas coisas\\Programação\\eclipse\\JavaUdemy\\src\\Texto Teste.txt";
 
-		try (BufferedReader br = new BufferedReader(new FileReader(path));){
-			
-			String line=br.readLine();
-			
-			while(line!= null) {
-				System.out.println(line);
-				line=br.readLine();
+		String[] lines = new String[] { "Sup ma' man", "Listenig to Lil Peep, hun?", "\n", "Good good" };
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-			
-		}catch(IOException e) {
-			System.out.println("Error "+e.getMessage());
+
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
 		}
-		
+
 	}
 
 }
