364782695ef37ccc621c1aa463ab64f10c808210
Davi Paulino <davipm@hotmail.com>
2022-10-24 11:40:34 -0300

    exercicio file

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,27 +1,29 @@
 package aulas;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Aulas {
 
 	public static void main(String[] args) {
 
-		String path = "C:\\Users\\AdaumirM\\Downloads\\minhas coisas\\Programação\\eclipse\\JavaUdemy\\src\\Texto Teste.txt";
-
-		String[] lines = new String[] { "Sup ma' man", "Listenig to Lil Peep, hun?", "\n", "Good good" };
-
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-
-		} catch (IOException e) {
-			System.out.println("Error " + e.getMessage());
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		sc.close();
 
 	}
 
--- exercicios/Exercicios.java
+++ exercicios/Exercicios.java
@@ -1,9 +1,39 @@
 package exercicios;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Exercicios {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+
+		StringBuffer sb = new StringBuffer();
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\AdaumirM\\Downloads\\minhas coisas\\Programação\\eclipse\\JavaUdemy\\src\\exercicios\\summary.csv"))) {
+			for (int i = 0; i < 4; i++) {
+
+				System.out.println("Product name, price and quantity: ");
+				String[] product = sc.nextLine().split(",");
+
+				Double price = Double.parseDouble(product[1]) * Double.parseDouble(product[2]);
+
+				sb.append(product[0] + "," + (double) price + "\n");
+
+				bw.write(product[0] + "," + (double) price);
+				bw.newLine();
+
+			}
+		} catch (IOException e) {
+
+			System.out.println("Erro " + e.getMessage());
+
+		}
+		System.out.println(sb);
+
+		sc.close();
 	}
 
 }
