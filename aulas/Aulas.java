271d810be4df747c65a2dfe92ad5b2cffaf8ba07
Davi Paulino <davipm@hotmail.com>
2022-11-04 11:32:41 -0300

    Exercicio Map< >

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,12 +1,25 @@
 package aulas;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Aulas {
 
 	public static void main(String[] args) {
 
-		 
-		
-		
+		Map<String, String> cook = new TreeMap<>();
+
+		cook.put("Saudação", "Olá");
+		cook.put("pronome ", "seu bosta");
+		cook.put("jk", "brinks");
+
+		cook.remove("jk");
+
+		System.out.println("Cookies ");
+		for (String keys : cook.keySet()) {
+			System.out.println(keys + ": " + cook.get(keys));
+		}
+
 	}
 
 }
--- exercicios/Exercicios.java
+++ exercicios/Exercicios.java
@@ -1,46 +1,49 @@
 package exercicios;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Exercicios {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Map<String, Integer> vote = new HashMap<>();
 
-		Set<Integer> a = new HashSet<>();
-		System.out.print("How many students for course A? ");
-		int num = sc.nextInt();
+		System.out.print("Enter file full path: ");
 
-		for (int i = 0; i < num; i++) {
-			a.add(sc.nextInt());
+		String path = "C:\\Users\\AdaumirM\\Downloads\\minhas coisas\\Programação\\eclipse\\JavaUdemy\\src\\Texto Teste.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] field = line.split(",");
+
+				String name = field[0];
+				Integer value = Integer.parseInt(field[1]);
+
+				if (vote.containsKey(name)) {
+					int votesSoFar = vote.get(name);
+					vote.put(name, value + votesSoFar);
+
+				} else {
+					vote.put(name, value);
+				}
+
+				line = br.readLine();
+
+			}
+
+			for (String keys : vote.keySet()) {
+				System.out.println(keys + ": " + vote.get(keys));
+			}
+
+		} catch (IOException e) {
+			e.getMessage();
 		}
 
-		System.out.print("How many students for course B? ");
-		num = sc.nextInt();
-		
-		Set<Integer> b = new HashSet<>();		
-		for (int i = 0; i < num; i++) {
-			b.add(sc.nextInt());
-		}
-
-		System.out.print("How many students for course C? ");
-		num = sc.nextInt();
-		
-		Set<Integer> c = new HashSet<>();	
-		for (int i = 0; i < num; i++) {
-			c.add(sc.nextInt());
-		}
-
-		Set<Integer> list = new HashSet<>(a);
-		list.addAll(b);
-		list.addAll(c);
-		
-		System.out.println("Total students: " + list.size());
-		
-
-		sc.close();
 	}
 
 }
