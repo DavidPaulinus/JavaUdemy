fdf7384b90f8918ef5abde778df741d74131f5b3
Davi Paulino <davipm@hotmail.com>
2022-11-08 10:18:42 -0300

    using predicate

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,23 +1,42 @@
 package aulas;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import aulas.entities.Product;
 
 public class Aulas {
 
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
 	public static void main(String[] args) {
 
-		Map<String, String> cook = new TreeMap<>();
+		List<Product> list = new ArrayList<>();
 
-		cook.put("Saudação", "Olá");
-		cook.put("pronome ", "seu bosta");
-		cook.put("jk", "brinks");
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
 
-		cook.remove("jk");
+		double min = 100.0;
+		
+		Predicate<Product> pred = x -> x.getPrice() >= min;
 
-		System.out.println("Cookies ");
-		for (String keys : cook.keySet()) {
-			System.out.println(keys + ": " + cook.get(keys));
+		list.removeIf(pred);
+
+		// list.removeIf(Product::nonStaticProductPredicate);
+
+		// list.removeIf(Product::staticProductPredicate);
+
+		// list.removeIf(new ProductPredicate());
+
+		// list.removeIf(p -> p.getPrice() >= 100);
+
+		for (Product p : list) {
+			System.out.println(p);
 		}
 
 	}
---
+++ aulas/util/ProductPredicate.java
@@ -0,0 +1,16 @@
+package aulas.util;
+
+import java.util.function.Predicate;
+
+import aulas.entities.Product;
+
+public class ProductPredicate implements Predicate<Product>{
+
+	@Override
+	public boolean test(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+	
+	
+	
+}
