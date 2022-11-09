2c4bdc3c95d700e9a64dd01ba61e2b71649ebcbe
Davi Paulino <davipm@hotmail.com>
2022-11-09 11:19:16 -0300

    pipeline demo

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,44 +1,26 @@
 package aulas;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import aulas.entities.Product;
-import aulas.util.ProductService;
 
 public class Aulas {
 
 	public static void main(String[] args) {
 
-		List<Product> list = new ArrayList<>();
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 
-		List<Integer> listint = Arrays.asList(1, 2, 3, 4, 5);
-
-		list.add(new Product("Tv", 900.00));
-		list.add(new Product("Mouse", 50.00));
-		list.add(new Product("Tablet", 350.50));
-		list.add(new Product("HD Case", 80.90));
-
-		ProductService ps = new ProductService();
-		System.out.println(Arrays.toString(list.toArray()));
-
-		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
-		System.out.println("Sum = " + String.format("%.2f", sum));
-
-		Stream<Product> str = list.stream();
-		Stream<Integer> str1 = listint.stream().map(x -> x + 3);
-
-		System.out.println(Arrays.toString(str.toArray()));
+		Stream<Integer> str1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(str1.toArray()));
 
-		Stream<String> str2 = Stream.of("José", "Maria");
-
-		Stream<Integer> str3 = Stream.iterate(0, x -> x + 2);
-
-		System.out.println(Arrays.toString(str3.limit(11).toArray()));
-
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		
+		System.out.println("Sum = "+sum);
+		
+		List<Integer> list2 = list.stream().filter(x ->x % 2 ==0).map(x -> x*10).collect(Collectors.toList());
+		
+		System.out.println(Arrays.toString(list2.toArray()));
 	}
 
 }
