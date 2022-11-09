939190804ca6841001b5767b91974637df9f381c
Davi Paulino <davipm@hotmail.com>
2022-11-09 12:29:04 -0300

    Exercicio resolvido filter,sorted,map

--- aulas/Aulas.java
+++ aulas/Aulas.java
@@ -1,26 +1,56 @@
 package aulas;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import aulas.entities.Product;
 
 public class Aulas {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		String path = "C:\\Users\\AdaumirM\\Downloads\\minhas coisas\\Programação\\eclipse\\JavaUdemy\\src\\Texto Teste.txt";
 
-		Stream<Integer> str1 = list.stream().map(x -> x * 10);
-		System.out.println(Arrays.toString(str1.toArray()));
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-		int sum = list.stream().reduce(0, (x, y) -> x + y);
-		
-		System.out.println("Sum = "+sum);
-		
-		List<Integer> list2 = list.stream().filter(x ->x % 2 ==0).map(x -> x*10).collect(Collectors.toList());
-		
-		System.out.println(Arrays.toString(list2.toArray()));
+			List<Product> list = new ArrayList<>();
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] prod = line.split(",");
+				list.add(new Product(prod[0], Double.parseDouble(prod[1])));
+
+				line = br.readLine();
+			}
+
+			Double avg = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x, y) -> x + y) / list.size();
+
+			System.out.println("Average Price: $ " + avg);
+			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+			List<String>name = list.stream()
+					.filter(x -> x.getPrice() <= avg)
+					.map(x -> x.getName())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+			
+			name.forEach(System.out::println);
+
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+
+		sc.close();
 	}
 
 }
